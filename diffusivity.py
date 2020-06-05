5900fb713f750356844522f94549b70eac36f08b
Ploch Noah <Noah.Ploch@wsi.tum.de>
2020-06-05 20:43:55 +0200

    Class skeleton

---
+++ diffusivity.py
@@ -0,0 +1,46 @@
+class DiffusivityMeasurement:
+    def __init__(self, filename):
+        pass
+
+    def read_file(filename):
+        pass
+
+    def R_vs_B():
+        pass
+
+    def R_vs_T(arg):
+        pass
+
+    def Bc2_vs_T(arg):
+        pass
+
+    def calc_diffusivity(arg):
+        pass
+
+
+    class Bc2vsTfit(object):
+        """docstring for Bc2vsTfit."""
+
+        def __init__(self, arg):
+            super(Bc2vsTfit, self).__init__()
+            self.arg = arg
+
+        def linear_fit(arg):
+            pass
+
+
+    class RTfit(object):
+        """docstring for RTfit."""
+
+        def __init__(self, arg):
+            super(RTfit, self).__init__()
+            self.arg = arg
+
+        def fit_function_parameters(arg):
+            pass
+
+        def fit_function(arg):
+            pass
+
+        def Tc(arg):
+            pass
