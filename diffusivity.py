1cb5d64ea9b0a293a8846d3f9d40da53166fb6d2
np-developer01 <ga87riw@mytum.de>
2020-06-08 20:48:50 +0200

    Implement first functions in DiffMeas class

--- diffusivity.py
+++ diffusivity.py
@@ -1,12 +1,33 @@
+from import_lib import *
+
 class DiffusivityMeasurement:
-    def __init__(self, filename):
-        pass
+    def __init__(self, filename): # to see why it is cleaner to create new object
+                                  # https://stackoverflow.com/questions/29947810/reusing-instances-of-objects-vs-creating-new-ones-with-every-update
+        self.filename = filename
+        self.key_w_data = 'data'
+        self._raw_data = self.__import_file_mat()
 
-    def read_file(filename):
-        pass
+        self.diffusivity = 0
+        self.diffusivity_err = 0
+        self.default_B_array = []
+        self.B_bin_size = 0.01
+        self.sheet_resistance_geom_factor = 4.53
+        self.sheet_resistance = 0
 
-    def R_vs_B():
-        pass
+
+    def __import_file_mat(self):
+        """reads string with filename and returns .mat data file as list of lists"""
+        return io.loadmat(self.filename, squeeze_me=True)[self.key_w_data]
+
+    def time_temperature_mapping():
+
+
+    def R_vs_B(B_sweep_index, err=False):
+        # TODO: implement when err=True
+        B_array_position = 9
+        R_array_position = 10
+        sweep = self._raw_data[B_sweep_index]
+        return (sweep[B_array_position], sweep[R_array_position])
 
     def R_vs_T(arg):
         pass
@@ -29,18 +50,21 @@ class DiffusivityMeasurement:
             pass
 
 
-    class RTfit(object):
-        """docstring for RTfit."""
+class RTfit(object):
+    """docstring for RTfit."""
 
-        def __init__(self, arg):
-            super(RTfit, self).__init__()
-            self.arg = arg
+    def __init__(self, arg):
+        super(RTfit, self).__init__()
+        self.arg = arg
 
-        def fit_function_parameters(arg):
-            pass
+    def fit_function_parameters(arg):
+        pass
 
-        def fit_function(arg):
-            pass
+    def fit_function(arg):
+        pass
 
-        def Tc(arg):
-            pass
+    def Tc(arg):
+        pass
+
+
+T=DiffusivityMeasurement('./testing_meas/200212_200109A_diffusweep.mat')
