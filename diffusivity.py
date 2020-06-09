fb672b54b8e743b62b3a45f8e095c0a3fad0f5b9
Ploch Noah <Noah.Ploch@wsi.tum.de>
2020-06-09 18:26:26 +0200

    Update diffusivity.py 09.06.2020

--- diffusivity.py
+++ diffusivity.py
@@ -6,10 +6,16 @@ class DiffusivityMeasurement:
         self.filename = filename
         self.key_w_data = 'data'
         self._raw_data = self.__import_file_mat()
+        self.index_t = 8
+        self.index_B = 9
+        self.index_R = 10
+        self.time_sweeps = [i[index_t] for i in self._raw_data]
+        self.B_sweeps = [i[index_R] for i in self._raw_data]
+        self.R_sweeps = [i[index_B] for i in self._raw_data]
 
         self.diffusivity = 0
         self.diffusivity_err = 0
-        self.default_B_array = []
+        self.default_B_array = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
         self.B_bin_size = 0.01
         self.sheet_resistance_geom_factor = 4.53
         self.sheet_resistance = 0
@@ -19,15 +25,35 @@ class DiffusivityMeasurement:
         """reads string with filename and returns .mat data file as list of lists"""
         return io.loadmat(self.filename, squeeze_me=True)[self.key_w_data]
 
-    def time_temperature_mapping():
+    def R_vs_T(self, B=None, err=False):
+
+        TBR_dict = {'T': self.__flatten_list(self.__time_temperature_mapping()),
+                    'B': self.__flatten_list(self.B_sweeps)
+                    'R': self.__flatten_list(self.R_sweeps)}
+        TBR_dict['B'] = self.__round_to_base(TBR_dict['B'], base=self.B_bin_size)
+        for t, b, r in zip (TBR_dict['T'], TBR_dict['B'], TBR_dict['R']):
 
 
-    def R_vs_B(B_sweep_index, err=False):
+    def __flatten_list(self, array):
+        return np.concatenate(array).ravel()
+
+    def __round_to_base(self, x, base=.005):
+      prec = str(base)[::-1].find('.')
+      return np.round(base * np.round(x/base),prec)
+
+    def __time_temperature_mapping(self): # TODO: what do we do with ths function, maybe change the whole data structure a bit
+        Temp_at_timestamp = [i[0:4] for i in self._raw_data]
+        T_array = []
+        for i,j in zip(self.time_sweeps, Temp_at_timestamp):
+            t = [i[0], i[-1]]
+            T = [j[2], j[3]]
+            p = np.polyfit(t,T,1)
+            T_array.append(np.polyval(p,i))
+        return T_array
+
+    def R_vs_B(self, B_sweep_index, err=False):
         # TODO: implement when err=True
-        B_array_position = 9
-        R_array_position = 10
-        sweep = self._raw_data[B_sweep_index]
-        return (sweep[B_array_position], sweep[R_array_position])
+        return (self.B_sweeps[B_sweep_index], self.R_sweeps[B_sweep_index])
 
     def R_vs_T(arg):
         pass
@@ -68,3 +94,5 @@ class RTfit(object):
 
 
 T=DiffusivityMeasurement('./testing_meas/200212_200109A_diffusweep.mat')
+a = T.R_vs_B(2)
+print(a)
